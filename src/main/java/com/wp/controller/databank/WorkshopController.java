b5c3fecc2f64460ebbac99142e0cea38ec32789e
wp <214639559@qq.com>
2018-03-14 17:14:37 +0800

    添加车间管理

---
+++ src/main/java/com/wp/controller/databank/WorkshopController.java
@@ -0,0 +1,7 @@
+package com.wp.controller.databank;
+
+/**
+ * Created by wp on 2018/3/14.
+ */
+public class workshop {
+}
---
+++ src/main/java/com/wp/service/databank/WorkshopService.java
@@ -0,0 +1,7 @@
+package com.wp.service.databank;
+
+/**
+ * Created by wp on 2018/3/14.
+ */
+public class WorkshopService {
+}
